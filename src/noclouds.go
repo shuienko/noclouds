8b92c712b6d657637014839873d9bac4cfb6f9ee
Oleksandr Shuienko <oleksandr.shuienko@gmail.com>
2023-12-07 20:44:47 +0100

    save

--- src/noclouds.go
+++ src/noclouds.go
@@ -243,27 +243,62 @@ func getAllStartPoints() DataPoints {
 // 	s.StartAsync()
 // }
 
+func handleStart(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	var numericKeyboard = tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Прогноз на 7 днів"),
+		),
+	)
+
+	if update.Message != nil {
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.IsCommand() && update.Message.Command() == "start" {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Розпочнімо. Тицяй кнопки!")
+			msg.ReplyMarkup = numericKeyboard
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		}
+	}
+}
+
+func handle7D(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	var numericKeyboard = tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Прогноз на 7 днів"),
+		),
+	)
+
+	if update.Message != nil {
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.Text == "Прогноз на 7 днів" {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, getAllStartPoints().Print())
+			msg.ReplyMarkup = numericKeyboard
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		}
+	}
+}
+
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_BOT_TOKEN"))
 	if err != nil {
 		log.Panic(err)
 	}
-
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil {
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, getAllStartPoints().Print())
-			bot.Send(msg)
-		}
+		handleStart(bot, update)
+		handle7D(bot, update)
 	}
 }
