7d015db884daf46e5fecb2009360b8c2c5304604
Oleksandr Shuienko <oleksandr.shuienko@gmail.com>
2024-12-17 23:14:20 +0100

    Fix Timezone issue

--- src/open-meteo.go
+++ src/open-meteo.go
@@ -62,6 +62,7 @@ func (response *OpenMeteoAPIResponse) Init(apiEndpoint, parameters, lat, lon str
 	params.Add("latitude", lat)
 	params.Add("longitude", lon)
 	params.Add("hourly", parameters)
+	params.Add("timezone", "auto")
 
 	// Make request to Open-Meteo API
 	req, err := http.NewRequest("GET", apiEndpoint+params.Encode(), nil)
